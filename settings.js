132a3975cbe179167449f0af0e440c2a6185c7a6
AbdulAhad000 <77685160+AbdulAhad000@users.noreply.github.com>
2023-06-06 23:15:05 +0530

    update settings.js

---
+++ settings.js
@@ -0,0 +1,27 @@
+const Settings = {};
+const Extracts = function(){
+    return Settings.enableExtracts().Extracts();
+}
+Settings.enableExtracts = function(){
+    Settings.Extracts = {};
+    return {"Extracts":function(){return Settings.Extracts},"clearExtracts":function(){Settings.Extracts = {};return Settings.Extracts;},"disableExtracts":function(){return Settings.disableExtracts()}}
+}
+Settings.disableExtracts = function(){
+    if(Settings.Extracts)
+    {
+    Settings.Extracts = undefined;
+    }
+    return {"Extracts":function(){return Settings.Extracts},"clearExtracts":function(){Settings.Extracts = {};return Settings.Extracts;},"disableExtracts":function(){return Settings.disableExtracts()}}
+}
+const Resources = function(){return Settings.enableResources().Resources()};
+Settings.enableResources = function(){
+    Settings.Resources = {};
+    return {"Resources":function(){const values = {}; return values;},"disableResources":function(){return Settings.disableResources()}}
+};
+Settings.disableResources = function(){
+    if(Settings.Resources)
+    {
+    Settings.Resources = undefined;
+    }
+    return {"Resources":function(){return Settings.Resources},"disableResources":function(){return Settings.disableResources()}}
+};
