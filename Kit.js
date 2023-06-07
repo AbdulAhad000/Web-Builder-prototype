9f75eaf3b708e4b167400c085d6e446ce3ae3025
AbdulAhad000 <77685160+AbdulAhad000@users.noreply.github.com>
2023-06-08 04:24:11 +0530

    update Kit.js

--- Kit.js
+++ Kit.js
@@ -1,131 +1,123 @@
-const Kit = {
-  "root": {
-    "Item": {
-      "items": {}
-    },
-    "methods": function() {
-      return {
-        "assignGroup": function(group) {
-          Object.assign(Kit.root.Item.items, {
-            ...group
-          });
-          return Kit.root.Item.items;
-        },
-        "clearGroup": function(key) {
-          Kit.root.Item.items[key] = {};
-          return Kit.root.Item.items;
-        },
-        "replaceItem": function(id, value) {
-          for (let group in Kit.root.Item.items) {
-            if (Kit.root.Item.items.hasOwnProperty(group)) {
-              const itemToUpdate = Kit.root.Item.items[group].find(
-                item => item.id === id
-              );
-              if (itemToUpdate) {
-                itemToUpdate.value = value;
-                return Kit.root.Item.items;
-              }
-            }
-          }
-          return Kit.root.Item.items;
-        },
-        "createGroup": function(groupName, ...items) {
-          return {
-            [groupName]: [...items]
-          };
-        },
-        "createItem": function(item) {
-          return {
-            id: item.id,
-            value: item.value
-          };
-        },
-        "getItem": function(id) {
-          for (let group in Kit.root.Item.items) {
-            if (Kit.root.Item.items.hasOwnProperty(group)) {
-              const item = Kit.root.Item.items[group].find(
-                item => item.id === id
-              );
-              if (item) {
-                return item.value;
-              }
-            }
-          }
-          return null;
-        },
-        "getItemsByGroup": function(groupName) {
-          return Kit.root.Item.items[groupName] || [];
-        },
-        "getGroup": function(groupName) {
-          return Kit.root.Item.items[groupName] || {};
-        },
-        "pushItem": function(groupName, item) {
-          Kit.root.Item.items[groupName].push(item);
-        }
-      };
-    }
-  },
-  "configure": {
-    "getExtracts": function(key, extracts) {
-      return Kit.configure.setExtracts(key, extracts).getExtracts();
-    },
-    "setExtracts": function(key, extracts) {
-      root.extracts = {};
-      Object.assign(Kit.configure, extracts, {
-        [key]: {
-          ...extracts
-        }
-      });
-      return {
-        "getExtracts": function() {
-          return root.extracts[key];
-        }
-      };
-    },
-    "setResource": function() {
-      root.Resource = {
-        values: {}
-      };
-      return {
-        "setValue": function(key, bundle) {
-          return Kit.configure.setValue(key, bundle);
-        }
-      };
-    },
-    "setBundle": function() {
-      Kit.configure.setValue = function(key, bundle) {
-        Object.assign(root.Resource.values, {
-          [key]: {
-            ...bundle
-          }
-        });
-        return {
-          "getValue": function() {
-            return Kit.configure.getValue(key);
-          },
-          "extendBundle": function(bundle) {
-            Object.assign(root.Resource.values[key], {
-              ...bundle
-            });
-            return {
-              "extendBundle": function(bundle) {
-                return Kit.configure.getValue().extendBundle(bundle);
-              }
-            };
-          }
-        };
-      };
-      Kit.configure.getValue = function(key) {
-        return root.Resource.values[key];
-      };
-      return {
-        "setValue": function(key, bundle) {
-          return Kit.configure.setValue(key, bundle);
-        },
-        "getValue": function(key) {
-          return Kit.configure.getValue(key);
-        }
-      };
-    }
+class Kit {
+  constructor() {
+    this.root = new Root();
+    this.configure = new Configure();
   }
-};
+}
+
+class Root {
+  constructor() {
+    this.Item = new Item();
+    this.extracts = {};
+    this.Resource = new Resource();
+  }
+}
+
+class Item {
+  constructor() {
+    this.items = {};
+  }
+
+  assignGroup(group) {
+    Object.assign(this.items, group);
+    return this;
+  }
+
+  clearGroup(key) {
+    this.items[key] = {};
+    return this;
+  }
+
+  replaceItem(id, value) {
+    for (let group in this.items) {
+      if (this.items.hasOwnProperty(group)) {
+        const itemToUpdate = this.items[group].find(item => item.id === id);
+        if (itemToUpdate) {
+          itemToUpdate.value = value;
+          return this;
+        }
+      }
+    }
+    return this;
+  }
+
+  createGroup(groupName, ...items) {
+    return {
+      [groupName]: [...items]
+    };
+  }
+
+  createItem(item) {
+    return {
+      id: item.id,
+      value: item.value
+    };
+  }
+
+  getItem(id) {
+    for (let group in this.items) {
+      if (this.items.hasOwnProperty(group)) {
+        const item = this.items[group].find(item => item.id === id);
+        if (item) {
+          return item.value;
+        }
+      }
+    }
+    return null;
+  }
+
+  getItemsByGroup(groupName) {
+    return this.items[groupName] || [];
+  }
+
+  getGroup(groupName) {
+    return this.items[groupName] || {};
+  }
+
+  pushItem(groupName, item) {
+    this.items[groupName].push(item);
+    return this;
+  }
+}
+
+class Configure {
+  constructor() {
+    this.root = new Root();
+  }
+
+  setExtracts(key, extracts) {
+    this.root.extracts = { ...this.root.extracts, [key]: extracts };
+    return this;
+  }
+
+  getExtracts(key) {
+    return this.root.extracts[key];
+  }
+
+  setResource() {
+    this.root.Resource = new Resource();
+    return this;
+  }
+
+  setValue(key, bundle) {
+    this.root.Resource.values[key] = { ...bundle };
+    return this;
+  }
+
+  getValue(key) {
+    return this.root.Resource.values[key];
+  }
+
+  extendBundle(key, bundle) {
+    Object.assign(this.root.Resource.values[key], bundle);
+    return this;
+  }
+}
+
+class Resource {
+  constructor() {
+    this.values = {};
+  }
+}
+
+const kit = new Kit();
