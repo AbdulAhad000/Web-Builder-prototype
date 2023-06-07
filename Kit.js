41a637e722e8ae86878e47276208321914f41abb
AbdulAhad000 <77685160+AbdulAhad000@users.noreply.github.com>
2023-06-08 04:38:52 +0530

    update Kit.js

--- Kit.js
+++ Kit.js
@@ -11,7 +11,7 @@ class Root {
     this.Item = new Item();
     this.extracts = {};
     this.Resource = new Resource();
-    this.Utility(){
+    this.Utility = function(){
         return new Utility()
     }
   }
